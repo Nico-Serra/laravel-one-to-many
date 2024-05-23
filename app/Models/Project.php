99529b48c0e084b23d419d6a652b0c65d7afca54
Nico-Serra <nico.serra000@gmail.com>
2024-05-23 15:23:04 +0200

    add foreign on table and add method for relationship

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,10 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'slug', 'cover_image', 'link', 'project_date'];
+    protected $fillable = ['name', 'slug', 'cover_image', 'link', 'project_date', 'type_id'];
+
+    /**
+     * Get the types  that owns the Project
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function types()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
